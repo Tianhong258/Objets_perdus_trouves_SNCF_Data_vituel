426c4e20802bf4a9a6b40b8b884f46ebd6689320
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2023-11-22 14:35:39 +0100

    nouvelle structure du code

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const landingTitle = document.querySelector('.landing h1');
-
-    // Get today's date and format it as YYYY/MM/DD
+function getTodaysDate() {
     const today = new Date();
-    const formattedDate = `${today.getFullYear()}/${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getDate().toString().padStart(2, '0')}`;
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const day = String(today.getDate()).padStart(2, '0');
 
-    // Construct the API URL with the dynamically generated date
-    const apiUrl = `https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-gares/records?limit=1&refine=date%3A%22${formattedDate}%22`;
+    return `${year}-${month}-${day}`;
+}
 
-    fetch(apiUrl)
+function updateItemCount(elementId, count) {
+    document.getElementById(elementId).textContent = count;
+}
+
+function fetchItemsData(url, elementId, countField) {
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            const totalCount = data.total_count; // Retrieve the total count from the API response
-
-            // Update the text content of the first h1 tag with the total count
-            landingTitle.textContent = `Aujourd'hui, ${totalCount} objets ont été perdus sur le réseau SNCF`;
+            const itemCount = data[countField] || 0;
+            updateItemCount(elementId, itemCount);
         })
         .catch(error => {
-            console.error('Error fetching data:', error);
-            landingTitle.textContent = `Aujourd'hui, ??? objets ont été perdus sur le réseau SNCF`;
+            console.log(`Error fetching ${elementId} data:`, error);
         });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const todayDate = getTodaysDate();
+    const formattedDate = encodeURIComponent(todayDate); // Encoding the date for URL usage
+
+    const lostItemsUrl = `https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-gares/records?limit=1&refine=date%3A%22${formattedDate}%22`;
+    const returnedItemsUrl = `https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?limit=1&refine=date%3A%222023%22&refine=gc_obo_date_heure_restitution_c%3A%22${formattedDate}%22`;
+
+    fetchItemsData(lostItemsUrl, 'totalLost', 'total_count');
+    fetchItemsData(returnedItemsUrl, 'totalReturned', 'total_count');
 });
