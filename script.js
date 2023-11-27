628b1f48a36857047f7671f9f4142dbfbfde5858
Tianhong258 <huangtianhong258@gmail.com>
2023-11-27 09:46:05 +0100

    pourcentage des derniers 6 mois est affiché

--- script.js
+++ script.js
@@ -33,3 +33,43 @@ document.addEventListener('DOMContentLoaded', function() {
     fetchItemsData(lostItemsUrl, 'totalLost', 'total_count');
     fetchItemsData(returnedItemsUrl, 'totalReturned', 'total_count');
 });
+
+function sum(number){
+    let sum = 0
+    //console.log(number)
+    for (i=0; i<number.results.length; i++){
+        sum += number.results[i].count
+    }
+    return sum
+} //calculer les objets totals
+
+async function percentageSixMonths(){
+    let resultSumLost
+    let resultSumFined
+    await fetch("https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-gares/records?select=COUNT(*)%20as%20count&group_by=RANGE(date%2C%201%20month)%20as%20date&order_by=date%20DESC&limit=6")
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data)
+            resultSumLost = sum(data)
+           //console.log(resultSumLost)
+           return resultSumLost
+        })
+        .catch(error => {
+            console.log(`Error fetching resultSumLost data:`, error);
+        });
+    await fetch("https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?select=COUNT(*)%20as%20count&group_by=RANGE(date%2C%201%20month)%20as%20date&order_by=date%20DESC&limit=6")
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data)
+            resultSumFined = sum(data)
+           //console.log(resultSumFined)
+           return resultSumFined
+        })
+        .catch(error => {
+            console.log(`Error fetching resultSumFined data:`, error);
+        });
+    let percentage = (resultSumFined/resultSumLost*100).toFixed(2)
+    //console.log(percentage)
+    document.getElementById("percentageSixMonths").innerHTML = percentage
+    }
+percentageSixMonths()
